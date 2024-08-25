225523a6dbfe3ad215eee6eab686e9f6aad30303
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2024-08-25 11:20:27 +0700

    feat: create navbar mobile component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={quicksand.className}>
-        <header>
+        <header className='w-full'>
           <NavbarMobile />
         </header>
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
---
+++ src/components/NavbarMobile.tsx
@@ -0,0 +1,187 @@
+'use client'
+
+import { Briefcase, Calendar, CreditCard, MoveUpRight, Rocket, Search, X } from 'lucide-react';
+import Image from 'next/image'
+import React, { useRef, useState } from 'react'
+import { Button } from './ui/button';
+import Link from 'next/link';
+
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+import clsx from 'clsx';
+
+
+const NavbarMobile = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const ref = useRef(null);
+
+  return (
+    <nav className='relative py-4 px-8 flex justify-between items-center h-[66px] w-full'>
+      <Image
+        alt='Dicoding Indonesia'
+        src={'/new-ui-logo.png'}
+        width={32}
+        height={32}
+        className='object-contain'
+      />
+
+      <button
+        name='Button Menu'
+        className='flex flex-col justify-center items-center gap-1 w-[58px] h-[50px] py-2 px-4 hover:bg-black/10 rounded-[2px] group'
+        onClick={() => setIsOpen((prev) => !prev)}
+      >
+        <div className='w-[24px] h-[2px] bg-primary group-focus-within:rotate-[350deg] transition-all duration-500'></div>
+        <div className='w-[24px] h-[2px] bg-primary group-focus-within:rotate-[350deg] transition-all duration-500'></div>
+        <div className='w-[24px] h-[2px] bg-primary group-focus-within:rotate-[350deg] transition-all duration-500'></div>
+      </button>
+
+      <div className={clsx('fixed top-0 right-0 w-full h-[500px] bg-white transition-all duration-300', {
+        '-translate-y-[100%] opacity-50': !isOpen,
+        'translate-y-0 opacity-100': isOpen,
+      })}>
+        <div className='flex justify-between items-center p-6 w-full h-[98px]'>
+          <div className='relative w-[80%] h-[50px]'>
+            <Search className='absolute top-[18px] left-[15px] w-4 h-4 text-gray-400' />
+            <input
+              type="text"
+              name="cari-pelajaran"
+              id="cari-pelajaran"
+              aria-label='Apa yang ingin Anda pelajari?'
+              placeholder='Apa yang ingin Anda pelajari?'
+              className='py-[6px] pl-[40px] pr-[6x] outline-none ring-0 w-full h-full focus:border focus:border-primary transition-all duration-300 rounded-[4px] border border-input font-medium text-[0.9rem] text-gray-950'
+            />
+          </div>
+
+          <button
+            onClick={() => setIsOpen((prev) => !prev)}
+          >
+            <X />
+          </button>
+        </div>
+
+        <div className='flex justify-center w-full bg-gray-50 gap-4 p-6'>
+          <Button variant={'outline'} size={'lg'} className='text-[1rem] font-bold'>Masuk</Button>
+          <Button size={'lg'} className='text-[1rem] font-bold'>Daftar</Button>
+        </div>
+
+        <ul>
+          <li>
+            <Link
+              href='#'
+              className='flex justify-between py-4 px-6 w-full text-base text-primary-text font-bold hover:underline '>
+              Learning Path
+              <MoveUpRight className='w-4 h-4 text-gray-400' />
+            </Link>
+          </li>
+          <li>
+            <Accordion type="single" collapsible>
+              <AccordionItem value="item-1" className='border-none'>
+                <AccordionTrigger
+                  className='px-6 w-full text-base text-primary-text font-bold'
+                >Langganan</AccordionTrigger>
+                <AccordionContent
+                  className='px-6 w-full text-base text-primary-text'
+                >
+                  <Link
+                    href="#"
+                    className='flex items-center py-4 hover:bg-gray-200'
+                  >
+                    <CreditCard className='w-4 h-4' />
+                    <div className='w-full ml-4'>
+                      <p className='text-[.9rem] font-bold'>Biaya Langganan</p>
+                      <span className='text-[.9rem]'>Bebas pilih kelas dengan berlanggangan.</span>
+                    </div>
+                  </Link>
+                  <Link
+                    href="#"
+                    className='flex items-center py-4 hover:bg-gray-200'
+                  >
+                    <CreditCard className='w-4 h-4' />
+                    <div className='w-full ml-4'>
+                      <p className='text-[.9rem] font-bold'>Aktivasi Token</p>
+                      <span className='text-[.9rem]'>Aktifkan token belajar Academy.</span>
+                    </div>
+                  </Link>
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
+          </li>
+          <li>
+            <Accordion type="single" collapsible>
+              <AccordionItem value="item-1" className='border-none'>
+                <AccordionTrigger
+                  className='px-6 w-full text-base text-primary-text font-bold'
+                >Program</AccordionTrigger>
+                <AccordionContent
+                  className='px-6 w-full text-base text-primary-text'
+                >
+                  <Link
+                    href="#"
+                    className='flex items-center py-4 hover:bg-gray-200 text-[.9rem]'
+                  >
+                    Dicoding Bangung Negeri Batch 7
+                  </Link>
+                  <Link
+                    href="#"
+                    className='flex items-center py-4 hover:bg-gray-200 text-[.9rem]'
+                  >
+                    AWS Back-End Academy 2024
+                  </Link>
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
+          </li>
+          <li>
+            <Accordion type="single" collapsible>
+              <AccordionItem value="item-1" className='border-none'>
+                <AccordionTrigger
+                  className='px-6 w-full text-base text-primary-text font-bold'
+                >Lainnya</AccordionTrigger>
+                <AccordionContent
+                  className='px-6 w-full text-base text-primary-text'
+                >
+                  <Link
+                    href="#"
+                    className='flex items-center py-4 hover:bg-gray-200'
+                  >
+                    <Rocket className='w-4 h-4' />
+                    <div className='w-full ml-4'>
+                      <p className='text-[.9rem] font-bold'>Dicoding Challenge</p>
+                      <span className='text-[.9rem]'>Asah skill Anda dengan mengikuti challenge.</span>
+                    </div>
+                  </Link>
+                  <Link
+                    href="#"
+                    className='flex items-center py-4 hover:bg-gray-200'
+                  >
+                    <Briefcase className='w-4 h-4' />
+                    <div className='w-full ml-4'>
+                      <p className='text-[.9rem] font-bold'>Dicoding Job</p>
+                      <span className='text-[.9rem]'>Temukan pekerjaan sesuai skill Anda.</span>
+                    </div>
+                  </Link>
+                  <Link
+                    href="#"
+                    className='flex items-center py-4 hover:bg-gray-200'
+                  >
+                    <Calendar className='w-4 h-4' />
+                    <div className='w-full ml-4'>
+                      <p className='text-[.9rem] font-bold'>Dicoding Event</p>
+                      <span className='text-[.9rem]'>Ikuti event dan bangun jaringan developer.</span>
+                    </div>
+                  </Link>
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+export default NavbarMobile
