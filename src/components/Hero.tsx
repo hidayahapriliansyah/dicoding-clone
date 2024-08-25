ded034bbd054a962e4597becacc7e4533fc08123
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2024-08-25 14:46:12 +0700

    chore: responsive hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,19 +5,19 @@ import Image from 'next/image'
 const Hero = () => {
   return (
     <section
-      className='flex w-full max-w-[1140px] h-[402px]'
+      className='flex flex-col-reverse md:flex-row mt-[80px] gap-8 w-full max-w-[1140px] h-[402px]'
     >
-      <div className='w-[40%] h-full pt-[100px] px-[15px]'>
-        <h2 className='text-[2rem] text-primary-text font-medium mb-4'>Bangun Karirmu Sebagai Developer Profesional</h2>
-        <p className='mb-10'>Mulai belajar terarah dengan learning path</p>
+      <div className='w-full md:w-[40%] h-full md:pt-[100px] px-[15px]'>
+        <h2 className='text-[1.5rem] md:text-[2rem] text-primary-text font-medium mb-4'>Bangun Karirmu Sebagai Developer Profesional</h2>
+        <p className='mb-5 md:mb-10'>Mulai belajar terarah dengan learning path</p>
         <Button
           className='font-bold text-[1rem] px-6'
         >Belajar Sekarang</Button>
       </div>
 
-      <div className='relative flex items-center w-[60%]'>
+      <div className='relative flex items-center w-full h-full max-h-[216px] md:max-h-full md:w-[60%]'>
         <Image
-          alt=''
+          alt='Image'
           src={'https://dicoding-web-img.sgp1.cdn.digitaloceanspaces.com/original/commons/homepage-hero.png'}
           fill
           className='object-contain overflow-hidden w-full'
