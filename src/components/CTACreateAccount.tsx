845e969f1faee01b6b9387b5377d59bf27ab31e9
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2024-08-29 10:54:02 +0700

    chore: add margin

--- src/components/CTACreateAccount.tsx
+++ src/components/CTACreateAccount.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const CTACreateAccount = () => {
   return (
-    <section className='w-full px-[15px]'>
+    <section className='mt-[30px] mb-[80px] w-full px-[15px]'>
       <div className='flex flex-col justify-center items-center p-10 w-full max-w-[510px] md:max-w-[690px] lg:md:max-w-[1140px] mx-auto bg-primary rounded-md'>
         <h2 className='mb-5 text-white text-center font-medium text-2xl'>Tunggu apa lagi?</h2>
         <p className='text-center text-white mb-4'>Belajar lebih terarah dengan learning path dan code review</p>
